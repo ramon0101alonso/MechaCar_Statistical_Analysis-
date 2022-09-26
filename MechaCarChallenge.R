7bb6b4c542f57151a29e0bf4dda57555e91c03f1
Ramon Alonso <ramonalonso@Ramons-MacBook-Pro.local>
2022-09-25 23:45:57 -0400

    challenge 15

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -6,7 +6,34 @@ library(dplyr) #import library
 
 MechaCar <- read.csv(file='MechaCar_mpg.csv', check.names= F, stringsAsFactors = F) #read file
 
-lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar)
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar) #linear regression
 
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar)) #summary
+summary(model)
 
+#Deliverable 2
+Suspension <- read.csv (file='Suspension_coil.csv', check.names =F, stringsAsFactors = F)  #read file
 
+total_summary <- Suspension %>% summarize(Mean=mean(PSI),
+                                       Median=median(PSI),
+                                       Var=var(PSI),
+                                       Std_Dev=sd(PSI),
+                                       .groups = 'keep') 
+view(total_summary)
+
+lot_summary <- Suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), 
+                                        Median=(PSI), 
+                                        Variance=var(PSI), 
+                                        SD=sd(PSI),
+                                        .groups = 'keep') 
+view(lot_summary)
+
+#Deliverable 3
+t.test(Suspension$PSI, mu= 1500)  #t-test for all lots
+
+#t-test for individual lots
+t.test(subset(Suspension, Manufacturing_Lot=="Lot1")$PSI, mu= 1500)
+
+t.test(subset(Suspension, Manufacturing_Lot=="Lot2")$PSI, mu= 1500)
+
+t.test(subset(Suspension, Manufacturing_Lot=="Lot3")$PSI, mu= 1500)
