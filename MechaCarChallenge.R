35138b3c15af396d607a65154ba8996aa253796c
Ramon Alonso <ramonalonso@Ramons-MacBook-Pro.local>
2022-09-25 22:36:14 -0400

    module 15

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,2 +1,12 @@
 
+#moduel 15 Challenge
+
+#Deliverable 1
+library(dplyr) #import library
+
+MechaCar <- read.csv(file='MechaCar_mpg.csv', check.names= F, stringsAsFactors = F) #read file
+
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar)
+
+
 
